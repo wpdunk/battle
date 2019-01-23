175e9f762da009d4c7edeab1a1e36a3868c4624b
William Dunk <williamdunk@gmail.com>
2019-01-23 16:05:16 +0000

    added a friendly web helper

--- spec/features/name_entry_spec.rb
+++ spec/features/name_entry_spec.rb
@@ -1,19 +1,13 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
-    expect(page).to have_content 'Dave vs. Mittens'
+    sign_in_and_play
+    expect(page).to have_content 'TOASTER vs. WALL-E'
   end
 end
 
 feature 'View hit points' do
   scenario 'player 2 HP' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Player 2 HP = 100'
   end
 end
