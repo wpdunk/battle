fbf77b2c13dca0e7e11ad3bb02da00f332779492
William Dunk <williamdunk@gmail.com>
2019-01-23 15:53:21 +0000

    added view HP

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   enable :sessions
 
+  # def initialize
+  #   @hp = 100
+  # end
+
   get '/' do
     'Testing infrastructure working!'
     erb :index
@@ -11,12 +15,14 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
+    session[:p2_hp] = 100
     redirect '/play'
   end
 
   get '/play' do
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
+    @p2_hp = session[:p2_hp]
     erb :play
   end
 
--- spec/features/name_entry_spec.rb
+++ spec/features/name_entry_spec.rb
@@ -7,3 +7,13 @@ feature 'Enter names' do
     expect(page).to have_content 'Dave vs. Mittens'
   end
 end
+
+feature 'View hit points' do
+  scenario 'player 2 HP' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Player 2 HP = 100'
+  end
+end
