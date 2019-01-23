f1f8ff9ea61eda10ca0d74f827f927869ee16843
William Dunk <williamdunk@gmail.com>
2019-01-23 18:31:45 +0000

    implemented attack

--- spec/features/name_entry_spec.rb
+++ spec/features/name_entry_spec.rb
@@ -8,6 +8,18 @@ end
 feature 'View hit points' do
   scenario 'player 2 HP' do
     sign_in_and_play
-    expect(page).to have_content 'Player 2 HP = 100'
+    expect(page).to have_content 'WALL-E HP = 100'
+  end
+end
+
+# As Player 1,
+# So I can win a game of Battle,
+# I want to attack Player 2, and I want to get a confirmation
+
+feature 'Attacking' do
+  scenario 'confirm player 2 attack' do
+    sign_in_and_play
+    click_button 'Attack!'
+    expect(page).to have_content 'TOASTER attacked WALL-E'
   end
 end
